70a910a5dc441b58fbe54e3d02701ac924b1d0b6
Cooper Sullivan <thecoopersully@gmail.com>
2023-08-08 18:56:38 -0500

    Shorten to 17

--- calendar_min.py
+++ calendar_min.py
@@ -3,9 +3,7 @@ def get_first_day_year(given_day: int = -1) -> int:
     return given_day
 def month_name(month_int: int) -> str: return {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}.get(month_int)
 def days_in_month(month_int: int, year_int: int) -> int:
-    if month_int in (1, 3, 5, 7, 8, 10, 12): return 31
-    if month_int in (4, 6, 9, 11): return 30
-    return 29 if (year_int % 4 == 0 and year_int % 100 != 0) or (year_int % 400 == 0) else 28
+    return 31 if month_int in (1, 3, 5, 7, 8, 10, 12) else 30 if month_int in (4, 6, 9, 11) else 29 if (year_int % 4 == 0 and year_int % 100 != 0) or (year_int % 400 == 0) else 28
 def print_month(year: int, month: int, weekday: int) -> None:
     print(f'{month_name(month)} - {year}'.center(33), "\n", "-" * 33, '\nSun  Mon  Tue  Wed  Thu  Fri  Sat\n' + "     " * weekday, end='')
     for i in range(1, days_in_month(month, year) + 1): print(f'{str(i).rjust(3)}  ', end='') if (i + weekday) % 7 != 0 else print(f'{str(i).rjust(3)}  ')
